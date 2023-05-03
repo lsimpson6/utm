2a5e013af03f4b7434fdcff676a29b1d4474f9ac
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-05-03 16:04:20 -0400

    test 2

--- UtmConversions.js
+++ UtmConversions.js
@@ -4,15 +4,14 @@
     buttons.forEach(button => button.addEventListener('click', ()=>{
         let track = button.getAttribute('data-track-utm');
         
-        if(track == "true"){
             let baseUrl = window.location.toString();
-            let btnUrl = button.getAttribute('href');
             const t = '?';
   
             let index = baseUrl.indexOf('?');
             let params = baseUrl.slice(index);
-            button.getAttribute('href').replace(btnUrl, btnUrl.concat(params));
-        }
+
+            button.getAttribute("href");
+            button.setAttribute("href", btnUrl.concat(params));
   
     }));
 })();
