d102593197fcb48f21db1558bfcf7d9e8c0b45d4
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-05-04 14:04:52 -0400

    update grabbing parameters

--- UtmConversions.js
+++ UtmConversions.js
@@ -1,38 +1,23 @@
 (function(){
     var buttons = document.querySelectorAll('.cta-button');
+    var link;
 
     buttons.forEach(button => button.addEventListener('click', ()=>{
         let track = button.getAttribute('data-track-utm');
-        var url = window.location.href;
+        link = button.getAttribute('data-link')
+        var urlSearchParams = window.location.search;
+        var buttonUrl = button.getAttribute('href');
 
-        // check if url has ? and if track is true
-  		if(url.includes("?") && track == "true"){
-            // get url from button
-            var buttonUrl = button.getAttribute('href');
-            // pass window url, button url, and button clicked to get and set the parameters
-            getSetParams(url, buttonUrl, button);
+  		if(urlSearchParams != ""){
+            if(buttonUrl.includes('?')){
+                buttonUrl = link;
+            }
+            redirect(buttonUrl, urlSearchParams, button);
   		}
     }));
 
-    function getSetParams(url, buttonUrl, button){
-        // gets the parameters from url
-        var params = url.slice(url.indexOf('?'));
-        // checks if the button url already parameters
-        if(buttonUrl.includes("?")){
-            // replaces ? with &
-            params = params.replace('?', '&');
-        }
-        // passes button url, updated paramater string, and button click value
-        redirect(buttonUrl, params, button);
-    }
-
     function redirect(u, p, b){
-        // checks if url has parameters
-        if(u.includes('?')){
-            // removes parameters
-            u = u.slice(0, u.indexOf('?') + 1);
-        }
-        // sets the href of the button to the url + parameters
         b.setAttribute('href', u + p);
+    	console.log(u + p);
     }
 })();
